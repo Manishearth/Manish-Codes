c4d0b286da6afee0e64bb0f40b0fe4c50dcd06a2
Manish Goregaokar <manishsmail@gmail.com>
2012-05-03 18:21:56 +0530

    'math'

---
+++ StackExchange/MathJaxTags.user.js
@@ -0,0 +1,168 @@
+// ==UserScript==
+ // @name MathJax buttons
+ // @version 1.0
+ // @author Manish Goregaokar (http://stackapps.com/users/10098/manishearth)
+ // @description Adds math buttons to the SEeditor, including a "$" button, a "$$" button, an SI-unit-ify button (on science sites), and a "\ce{}" button on chem.SE
+ // @license GNU GPL v3 (http://www.gnu.org/copyleft/gpl.html) 
+ // @include http://*math.stackexchange.com/*
+ // @include http://physics.stackexchange.com/*
+ // @include http://chemistry.stackoverflow.com/*
+ // @include http://biology.stackoverflow.com/*
+ // ==/UserScript==
+
+
+//Some functionality copied from https://gist.github.com/2583075. Thanks, Brock.
+
+
+//Methods:
+
+SIify=clickButtonEventLambda("\\:\\mathrm{","}");
+dollarify=clickButtonEventLambda("$","$");
+Ddollarify=clickButtonEventLambda("$$","$$");
+chemify=clickButtonEventLambda("\\ce{","}");
+
+//Configuration:
+var buttonconfig={
+	"2 (SI)":["SI",SIify,"SI","","s",/(physics|chem|biology)/ig],
+	"1 (Dollar)":["$",dollarify,"dollar","","m",/stack/ig],
+	"4 (DoubleDollar)":["$$",Ddollarify,"Ddollar","","",/stack/ig],
+	"3 (Chem)":["O<sub>2</sub>",chemify,"chemify","","c",/chemistry/ig],
+};
+
+
+
+
+function addButton(text,callback,identify,pic){
+//Callback must take id of textarea as argument.
+$.each($('.wmd-container').not(".canhasbutton"+identify),function(){
+try{
+tid=$(this).find("[id^=wmd-input]")[0].id;
+row=$(this).find("[id^=wmd-button-row]")[0];
+lastel=$(row).find(".wmd-button").not(".wmd-help-button").filter(":last");
+if(lastel.length>0){
+px=parseInt(lastel[0].style.left.replace("px",""))+25;
+//add code for background-position of span as well later
+btn='<li class="wmd-button" style="left: '+px+'px; "><span style="background-image:url('+pic+');text-align:center;">'+text+'</span></li>';
+$(btn).on("click",function(){callback(tid)}).insertAfter(lastel);
+btn=$(row).find(".wmd-button").not(".wmd-help-button").filter(":last");
+if(pic==""){
+$(btn).children('span').hover(function(){$(this).css('background','#DEEDFF')},function(){$(this).css('background','#FFFFFF')});
+}
+}
+this.className+=" canhasbutton"+identify;
+}catch(e){console.log(e)}
+})
+}
+
+
+
+
+
+
+
+
+function keyPressEventLambda(key,callback){
+return function (zEvent) {
+		//console.log(zEvent);
+        //--- On Alt-K, insert the <kbd> set. Ignore all other keys.
+        if (zEvent.altKey  && ( zEvent.which == key.charCodeAt(0)||zEvent.which == key.toUpperCase().charCodeAt(0))) {
+            callback(this.id);
+            return false;
+        }
+        return true;
+}
+}
+
+function clickButtonEventLambda(left, right){
+ return function (tid) {
+	 
+	 	node=$('#'+tid)[0];
+		
+		try{
+        //--- Wrap selected text or insert at curser.
+        var oldText         = node.value || node.textContent;
+        var newText;
+        var iTargetStart    = node.selectionStart;
+        var iTargetEnd      = node.selectionEnd;
+
+        if (iTargetStart == iTargetEnd)
+            newText         = left+right;
+        else
+            newText         = left + oldText.slice (iTargetStart, iTargetEnd) + right;
+
+        //console.log (newText);
+        newText             = oldText.slice (0, iTargetStart) + newText + oldText.slice (iTargetEnd);
+        node.value          = newText;
+        //-- After using spelling corrector, this gets buggered, hence the multiple sets.
+        node.textContent    = newText;
+        //-- Have to reset selection, since we repasted the text.
+        node.selectionStart = iTargetStart + left.length;
+        node.selectionEnd   = iTargetEnd   + right.length+left.length-1;
+			node.focus ();
+        try {
+            //--- This is a utility function that SE currently provides on its pages.
+            StackExchange.MarkdownEditor.refreshAllPreviews ();
+        }
+        catch (e) {
+            console.warn ("***Userscript error: refreshAllPreviews() is no longer defined!");
+        }
+		}catch (e) {
+            console.warn ("***Textarea does not exist");
+			console.log(e);
+        }
+    }
+
+
+}
+
+
+
+
+function addButtons(){
+	for(var i in buttonconfig){
+			if(window.location.host.match(buttonconfig[i][5])){
+				addButton(buttonconfig[i][0],buttonconfig[i][1],buttonconfig[i][2],buttonconfig[i][3])
+			}
+	}
+}
+
+function addKeyPressEventLives(){
+
+		for(var i in buttonconfig){
+			if(buttonconfig[i][4]!=""){
+				if(window.location.host.match(buttonconfig[i][5])){
+					$("textarea.wmd-input").live("keydown", keyPressEventLambda(buttonconfig[i][4],buttonconfig[i][1]));
+				}
+			}
+		}
+}
+
+
+function with_jquery(f) {
+     var script = document.createElement("script");
+     script.type = "text/javascript";
+     script.textContent = "(" + f.toString() + ")(jQuery)";
+     document.body.appendChild(script);
+};
+
+with_jquery(function($){
+$(document).ready(function(){
+addButtons()
+addKeyPressEventLives()
+$("textarea.wmd-input").live("click",addButtons);
+});
+
+
+
+
+
+
+window.waitUntilExists=function(wb,wfn){
+	if(wb.length==0){
+   		waitFunc=function(){waitUntilExists(wb,wfn)};
+		setTimeout(waitFunc,20);
+	}else{
+		wfn();
+	}
+}
+});
