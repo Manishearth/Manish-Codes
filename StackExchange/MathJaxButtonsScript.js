54e4b45b4af8212efc641b7c9da49b72ce21c742
Manishearth <manishsmail@gmail.com>
2012-05-05 21:07:44 +0530

    Add keyboard shortcuts

--- StackExchange/MathJaxButtonsScript.js
+++ StackExchange/MathJaxButtonsScript.js
@@ -1,4 +1,3 @@
-
 //clickbuttoneventlambda:
 window.clickButtonEventLambda=function(left, right){
  return function (tid) {
@@ -56,20 +55,20 @@ window.chemify=clickButtonEventLambda("$\\ce{","}$");
 
 //************************************************
 //Configuration:
-//format: [Button HTML text,callback,unique identifier,keyboard shortcut,regex for sitename,regex for NOT sitename, empty string if you don't need this]
+//format: [Button HTML text,callback,unique identifier,empty,keyboard shortcut,regex for sitename,regex for NOT sitename(empty string if you don't need this),tooltip]
 window.buttonconfig={
-	"2 (SI)":["SI",SIify,"SI","","s",/(physics|chem|biology|electronics)/ig,""],
-	"1 (Dollar)":["$",dollarify,"dollar","","m",/stack/ig,/electronics/ig],
-	"4 (DoubleDollar)":["$$",Ddollarify,"Ddollar","","",/stack/ig,/electronics/ig],
+	"2 (SI)":["SI",SIify,"SI","","s",/(physics|chem|biology|electronics)/ig,"","Format selection as an SI unit"],
+	"1 (Dollar)":["$",dollarify,"dollar","","m",/stack/ig,/electronics/ig,"Enclose in MathJax dollar symbols"],
+	"4 (DoubleDollar)":["$$",Ddollarify,"Ddollar","","",/stack/ig,/electronics/ig,"Enclose in MathJax double-dollar symbols"],
 
 	//Special per-site ones:
-	"3 (Chem)":["O<sub>2</sub>",chemify,"chemify","","c",/chemistry/ig,""],
-	"5 (Phy)":["<b>E</b>",clickButtonEventLambda("\\mathbf{","}"),"vectorfield","","v",/physics/ig,""],
+	"3 (Chem)":["O<sub>2</sub>",chemify,"chemify","","c",/chemistry/ig,"","Enclose selection in $\ce{...}$"],
+	"5 (Phy)":["<b>E</b>",clickButtonEventLambda("\\mathbf{","}"),"vectorfield","","v",/physics/ig,"","Format selection as vector field"],
 	//Electronics.SE needs a backslash
-	"1 (DollarElectronics)":["$",clickButtonEventLambda("\\$","\\$"),"dollar","","m",/electronics/ig,""],
-	"4 (DDollarElectronics)":["$$",Ddollarify,"Ddollar","","d",/electronics/ig,""], //keyboard shortcut
+	"1 (DollarElectronics)":["$",clickButtonEventLambda("\\$","\\$"),"dollar","","m",/electronics/ig,"","Enclose in MathJax dollar symbols"],
+	"4 (DDollarElectronics)":["$$",Ddollarify,"Ddollar","","d",/electronics/ig,"","Enclose in MathJax double-dollar symbols"], //keyboard shortcut
 	//Big O notation
-	"5 (BigO)":['<span style=style="font-family: MathJax_Caligraphic; ">O</span>',clickButtonEventLambda("$\\mathcal{O}(",")$"),"dollar","","o",/crypto/ig,""],	
+	"5 (BigO)":['<span style=style="font-family: MathJax_Caligraphic; ">O</span>',clickButtonEventLambda("$\\mathcal{O}(",")$"),"dollar","","o",/crypto/ig,"","Enclose selection in big O notation"],	
 };
 
 //************************************************
@@ -79,7 +78,7 @@ window.buttonconfig={
 
 
 
-window.addButton=function(text,callback,identify,pic,force){
+window.addButton=function(text,callback,identify,pic,tooltip,force){
 //Callback must take id of textarea as argument.
 force = typeof force !== 'undefined' ? force : false;
 var tas=force?$('.wmd-container'):$('.wmd-container').not(".canhasbutton"+identify);
@@ -99,7 +98,9 @@ if(lastel.length>0){
 px=parseInt(lastel[0].style.left.replace("px",""))+25;
 //add code for background-position of span as well later
 btn='<li class="wmd-button" style="left: '+px+'px; "><span style="background-image:url('+pic+');text-align:center;">'+text+'</span></li>';
-$(btn).on("click",function(){callback(tid)}).insertAfter(lastel);
+
+$(btn).on("click",function(){callback(tid)}).attr("title",tooltip).insertAfter(lastel);
+
 btn=$(row).find(".wmd-button").not(".wmd-help-button").filter(":last");
 if(pic==""){
 $(btn).children('span').hover(function(){$(this).css('background','#DEEDFF')},function(){$(this).css('background','none')});
@@ -136,7 +137,8 @@ return function (zEvent) {
 window.addButtons=function(){
 	for(var i in buttonconfig){
 			if(window.location.host.match(buttonconfig[i][5])&&(buttonconfig[i][6]==""||!window.location.host.match(buttonconfig[i][6]))){
-				addButton(buttonconfig[i][0],buttonconfig[i][1],buttonconfig[i][2],buttonconfig[i][3])
+				
+				addButton(buttonconfig[i][0],buttonconfig[i][1],buttonconfig[i][2],buttonconfig[i][3],(buttonconfig[i][7]+((buttonconfig[i][4])?" (Alt-"+buttonconfig[i][4].toUpperCase()+")":"")),false);
 			}
 	}
 }
@@ -145,8 +147,13 @@ window.addKeyPressEventLives=function(){
 
 		for(var i in buttonconfig){
 			if(buttonconfig[i][4]!=""){
+
 				if(window.location.host.match(buttonconfig[i][5])&&(buttonconfig[i][6]==""||!window.location.host.match(buttonconfig[i][6]))){
+					if(buttonconfig[i].keyAdded){
+						continue;
+					}
 					$("textarea.wmd-input").live("keydown", keyPressEventLambda(buttonconfig[i][4],buttonconfig[i][1]));
+					buttonconfig[i].keyAdded=true;
 				}
 			}
 		}
