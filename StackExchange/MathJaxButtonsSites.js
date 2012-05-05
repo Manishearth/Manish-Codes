8305d0b083e6de81ebb6763aedb794ac6f04bfe8
Manishearth <manishsmail@gmail.com>
2012-05-05 20:22:11 +0530

    Update StackExchange/MathJaxButtonsSites.js

--- StackExchange/MathJaxButtonsSites.js
+++ StackExchange/MathJaxButtonsSites.js
@@ -1,5 +1,8 @@
-//(function(){
-var x=window.location.origin;
+//Smaller script to reduce loading size/time on each SE site (since this must autoupdate when new sites are added)
+
+
+(function(){
+var x=window.location.hostname;
 
 //*************************************
 var sites=["[^\\/]*math[^\\/]*","physics","chemistry","biology","electronics","crypto","[^\\/]*quant","[^\\/]*stats"];	  
@@ -20,7 +23,7 @@ function importMainScript(){
 	 var d=new Date();
      var script = document.createElement("script");
      script.type = "text/javascript";
-     script.src="https://raw.github.com/Manishearth/Manish-Codes/master/StackExchange/MathJaxButtonsScript.js?rand="+Math.random();
+     script.src="https://raw.github.com/Manishearth/Manish-Codes/master/StackExchange/MathJaxButtonsScript.js"+d.getMonth()+""+d.getDay();
      document.body.appendChild(script);
 }
-//})();
+})();
