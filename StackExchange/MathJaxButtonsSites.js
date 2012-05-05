74df9205a3e9a682dd666950a07a4eff69671742
Manish Goregaokar <manishsmail@gmail.com>
2012-05-05 20:20:33 +0530

    'works'

--- StackExchange/MathJaxButtonsSites.js
+++ StackExchange/MathJaxButtonsSites.js
@@ -1,3 +1,5 @@
+//Smaller script to reduce loading size/time on each SE site (since this must autoupdate when new sites are added)
+
 
 (function(){
 var x=window.location.hostname;
@@ -21,7 +23,7 @@ function importMainScript(){
 	 var d=new Date();
      var script = document.createElement("script");
      script.type = "text/javascript";
-     script.src="https://raw.github.com/Manishearth/Manish-Codes/master/StackExchange/MathJaxButtonsScript.js"
+     script.src="https://raw.github.com/Manishearth/Manish-Codes/master/StackExchange/MathJaxButtonsScript.js"+d.getMonth()+""+d.getDay();
      document.body.appendChild(script);
 }
 })();
