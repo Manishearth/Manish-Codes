d09886af6226c84b07f3c8ca85fbf7166f0f7878
Manishearth <manishsmail@gmail.com>
2012-05-07 11:24:28 +0530

    Update StackExchange/MathJaxButtonsSites.js

--- StackExchange/MathJaxButtonsSites.js
+++ StackExchange/MathJaxButtonsSites.js
@@ -2,7 +2,7 @@
 
 
 (function(){
-var x=window.location.origin;
+var x="http://"+window.location.hostname;
 
 //*************************************
 var sites=["[^\\/]*math[^\\/]*","physics","chemistry","biology","electronics","crypto","[^\\/]*quant","[^\\/]*stats","dsp","cogsci","cs","[^\\/]*cstheory"];	  
