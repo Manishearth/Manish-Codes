e5f180e531ce5b5c683ad09120bb749a4b1893ef
Manish Goregaokar <manishsmail@gmail.com>
2012-05-15 15:06:36 +0530

    'working'

--- StackExchange/ThumbnailUpload/ThumbnailUpload.js
+++ StackExchange/ThumbnailUpload/ThumbnailUpload.js
@@ -34,25 +34,63 @@ ThumbnailUpload.showModal=function(tid){
 	$('<div id="thumbnail-upload-dialog" class="wmd-prompt-dialog" style="z-index:1001;position:absolute;left:'+(window.innerWidth-modalParams[0])/2+'px;top:'+(window.innerHeight-modalParams[1])/2+'px;width:'+modalParams[0]+'px;height:'+modalParams[1]+'px;"><iframe id="thumbnail-upload-iframe" style="width:100%;height:100%"></iframe></div>').appendTo('body');
 	ThumbnailUpload.iframeWin=$('#thumbnail-upload-iframe')[0].contentWindow;
 
+
 	 var script = iframeWin.document.createElement("script");
      script.type = "text/javascript";
      script.src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"
      iframeWin.document.head.appendChild(script);
-	 
 
-	 var script = iframeWin.document.createElement("script");
-     script.type = "text/javascript";
-     script.textContent="("+ThumbnailUpload.childScripts.toString()+")()";
-     iframeWin.document.body.appendChild(script);
+	function loadScr(){
+	 if(iframeWin.$){
+		 var script = iframeWin.document.createElement("script");
+		 script.type = "text/javascript";
+		 script.textContent="("+ThumbnailUpload.childScripts.toString()+")('"+StackExchange.options.user.fkey+"')";
+		 iframeWin.document.body.appendChild(script);
+	 }else{
+		setTimeout(loadScr,10); 
+	 }
+	}
+	loadScr();
+}
+ThumbnailUpload.closeModal=function(){
+	$('#thumbnail-upload-shadow, #thumbnail-upload-dialog').remove();
+}
+ThumbnailUpload.childScripts=function(fkey){
+var styles = document.createElement("link");
+styles.type = "text/css";
+styles.rel="stylesheet";
+styles.href=window.parent.$('link[rel=stylesheet]').filter(function(){if(!this.href){return false;}return this.href.match(/all\.css/)})[0].href;
+document.head.appendChild(styles);
+var ts=new Date().getTime()
+	
+	
+$('body').css('background-color',"#DDD").append('<p><b>Insert an image</b></p><p style="padding-top: 10px;"><a href="javascript:void(0)" class="wmd-mini-button selected" id="upload-image-button">from my computer</a><a href="javascript:void(0)" class="wmd-mini-button" id="upload-url-button">from the web</a></p>')
+.append('<form action="/upload/image" id="frompcupload" method="post" enctype="multipart/form-data"><input type="hidden" name="fkey" value="'+fkey+'"><div style="position: relative;height: 40px;" id="upload-file-input"><img src="http://i.imgur.com/GKc7H.png" height="15px" width="15px" style="vertical-align:middle;margin-right:6px;"><input type="file" name="filename" id="filename-input" value="browse" style="border:0; vertical-align:middle;"></div><p id="upload-message" style="padding-top: 4px; margin:0; line-height: 16px;">click browse to choose an image from your computer</p><div class="ac_loading" id="image-upload-progress" class="image-upload-progress" style="background-color: transparent; display:none;">Uploading ...</div><input type="submit" value="Upload" style="width: 7em; margin: 10px;"><input type="button" value="Cancel" id="close-dialog-button" class="close-dialog-button" style="width: 7em; margin: 10px 10px 20px;"></form>')
+.append('<form action="/upload/image" id="fromwebupload" style="display:none" method="post" enctype="multipart/form-data"><div id="upload-url-input" style="height: 40px; "><input type="input" name="upload-url" value="" style="width: 250px;"></div><input type="hidden" name="fkey" value="'+fkey+'"><p id="upload-message" style="padding-top: 4px; margin:0; line-height: 16px;">paste the URL of your image above</p><div class="ac_loading" id="image-upload-progress" class="image-upload-progress" style="background-color: transparent; display:none;">Uploading ...</div><input type="submit" value="Upload" style="width: 7em; margin: 10px;"><input type="button" value="Cancel" id="close-dialog-button" class="close-dialog-button" style="width: 7em; margin: 10px 10px 20px;"></form>').append('<iframe id="upload-iframe-'+ts+'" style="display:none;" src="about:blank" name="upload-iframe-'+ts+'"></iframe>');
+
+$('form').attr('target',"upload-iframe-"+ts);
+$("#upload-image-button").click(function(){$('#fromwebupload').hide();$('#frompcupload').show();$("#upload-image-button").addClass('selected');$("#upload-url-button").removeClass('selected');return false;})
+$("#upload-url-button").click(function(){$('#frompcupload').hide();$('#fromwebupload').show();$("#upload-image-button").removeClass('selected');$("#upload-url-button").addClass('selected');return false;})
+
+
+
+$('.close-dialog-button').click(function(){window.parent.ThumbnailUpload.closeModal();return false;});
+$('form').submit(function(){$('.ac_loading').show();return true;});
+	//closeDialog(result)
+    //displayUploadError(error)
+	
+	
+window.closeDialog=function(result){
+	console.log(["cD",result]);
+	
+}
+window.displayUploadError=function(error){
+	console.log(["dUE",error]);
 	
 }
 
-ThumbnailUpload.childScripts=function(){
-	document.write("aaaaa");
-	$("<div>AAAAA</div>").appendTo(document);
-	
-}
 
+}
 
 
 	
