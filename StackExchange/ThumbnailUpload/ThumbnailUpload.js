542b7bf20aef2a2fc735d5954363e725698f6c82
Manish Goregaokar <manishsmail@gmail.com>
2012-05-24 14:51:43 +0530

    Teachersloungeping

--- StackExchange/ThumbnailUpload/ThumbnailUpload.js
+++ StackExchange/ThumbnailUpload/ThumbnailUpload.js
@@ -2,32 +2,10 @@
 
 
 (function(){
-//<div class="wmd-prompt-background" style="position: absolute; top: 0px; z-index: 1000; opacity: 0.5; height: 836px; left: 0px; width: 100%; "></div>
-//http://stackoverflow.com/questions/6250704/view-image-selected-from-file-system-on-client-side-before-upload
-/*
 
-<div style="top: 234.5px; left: 474.5px; display: block; padding: 10px; position: absolute; width: 400px; z-index: 1001; " class="wmd-prompt-dialog">
-
-<div style="position: absolute; right: 20px; bottom: 5px; font-size: 10px;">image hosting by <a title="imgur: the simple image sharer" href="http://imgur.com">imgur.com</a></div>
-
-
-<p><b>Insert an image</b></p>
-
-<p style="padding-top: 10px;"><a href="#" class="wmd-mini-button selected" id="upload-image-button">from my computer</a>
-<a href="#" class="wmd-mini-button" id="upload-url-button">from the web</a></p>
-
-
-<form action="/upload/image" method="post" enctype="multipart/form-data"><input type="hidden" name="fkey" value="f98fbceda0a86f81d98ddad95771349e"><div style="position: relative;height: 40px;" id="upload-file-input"><img src="http://i.imgur.com/GKc7H.png" height="15px" width="15px" style="vertical-align:middle;margin-right:6px;">
-
-<input type="file" name="filename" id="filename-input" value="browse" style="border:0; vertical-align:middle;"></div><p id="upload-message" style="padding-top: 4px; margin:0; line-height: 16px;">click browse to choose an image from your computer</p><div class="ac_loading" id="image-upload-progress" style="background-color: transparent; display:none;">Uploading ...</div><input type="submit" value="Upload" style="width: 7em; margin: 10px;"><input type="button" value="Cancel" id="close-dialog-button" style="width: 7em; margin: 10px 10px 20px;"></form>
-
-</div>
-
-
-*/
 window.ThumbnailUpload={};
 with(ThumbnailUpload){
-ThumbnailUpload.modalParams=[350,200];//[width,height]
+ThumbnailUpload.modalParams=[400,300];//[width,height]
 
 ThumbnailUpload.showModal=function(tid){
 	$('<div id="thumbnail-upload-shadow" style="position:fixed;top:0;left:0;right:0;bottom:0;z-index:1000;opacity:0.5;background-color:black"></div>').appendTo('body');
@@ -78,8 +56,18 @@ $('.close-dialog-button').click(function(){window.parent.ThumbnailUpload.closeMo
 $('form').submit(function(){$('.ac_loading').show();return true;});
 	//closeDialog(result)
     //displayUploadError(error)
-	
-	
+$('<table style="text-align:center;font-size:10px;margin:0 auto;border-spacing:10px;border-collapse:inherit"><tr id="thumbupload-options"><td class="thumbupload-optionbx" id="thumbupload-optionbx-bypass"><input type=radio name="thumbupload-options-bx" id="thumbupload-option-bypass" selected><label for="thumbupload-option-bypass"> Normal image</label></td><td class="thumbupload-optionbx" id="thumbupload-optionbx-sizes"><table><tr><td><input type=radio name="thumbupload-options-bx" id="thumbupload-option-large"><label for="thumbupload-option-large"> Large</label></td></tr><tr><td><input type=radio name="thumbupload-options-bx" id="thumbupload-option-medium"><label for="thumbupload-option-medium"> Medium</label></td></tr><tr><td><input type=radio name="thumbupload-options-bx" id="thumbupload-option-small"><label for="thumbupload-option-small"> Small</label></td></tr></table></td><td id="thumbupload-optionbx-custom" class="thumbupload-optionbx"><table><tr><td><input type=text size=3 name="thumbupload-options-height" id="thumbupload-option-height"><label for="thumbupload-option-height"> Height(px)</label></td></tr><tr><td><input type=text size=3 name="thumbupload-options-width" id="thumbupload-option-width"><label for="thumbupload-option-width"> Width(px)</label></td></tr></table></td></tr></table>').appendTo('body')
+
+$('td.thumbupload-optionbx').css({
+"padding":"10px",
+"-webkit-border-radius":"7px",
+"-moz-border-radius":"7px"}).hover(function(){$(this).css("background-color","#AAA")},function(){if($(this).hasClass('selected')){$(this).css("background-color","#CCC")}else{$(this).css("background-color","#DDD")}}).click(function(){$('td.thumbupload-optionbx.selected').css("background-color","#DDD").removeClass('selected');$(this).css("background-color","#AAA").addClass('selected');})
+$('#thumbupload-optionbx-bypass').click(function(){$('#thumbupload-option-bypass').attr('checked',true);$("#thumbupload-option-height,#thumbupload-option-width").blur()}).click();
+$('#thumbupload-optionbx-sizes').click(function(){if(!$('#thumbupload-option-large').attr('checked')&&!$('#thumbupload-option-small').attr('checked'))$('#thumbupload-option-medium').attr('checked',true);$("#thumbupload-option-height,#thumbupload-option-width").blur()});
+$('#thumbupload-optionbx-custom').click(function(){$('input[type=radio]').attr('checked',false);$("#thumbupload-option-height").focus()});
+
+
+
 window.closeDialog=function(result){
 	$('.ac_loading').hide()
 	console.log(["cD",result]);
@@ -92,6 +80,9 @@ window.displayUploadError=function(error){
 }
 
 
+
+
+
 }
 
 
@@ -101,3 +92,4 @@ window.displayUploadError=function(error){
 
 
 })();
+
