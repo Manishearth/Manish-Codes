d45d0a3fb57065b232c08496e696e70ce231d732
Manish Goregaokar <manishsmail@gmail.com>
2012-05-29 06:34:55 +0530

    fix

--- StackExchange/ThumbnailUpload/ThumbnailUpload.user.js
+++ StackExchange/ThumbnailUpload/ThumbnailUpload.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name Thumbnail Uploader
-// @version 0.0.1
+// @version 0.0.2
 // @author Manish Goregaokar (http://stackapps.com/users/10098/manishearth)
 // @description Allows uploading of linked thumbnails for images
 // @license GNU GPL v3 (http://www.gnu.org/copyleft/gpl.html) 
@@ -15,7 +15,7 @@
 	 var d=new Date();
      var script = document.createElement("script");
      script.type = "text/javascript";
-     script.src="https://raw.github.com/Manishearth/Manish-Codes/master/StackExchange/MathJaxButtonsSites.js?rand="+d.getMonth()+""+d.getDay();
+     script.src="https://raw.github.com/Manishearth/Manish-Codes/master/StackExchange/ThumbnailUpload/ThumbnailUpload.js?rand="+d.getMonth()+""+d.getDay();
      if(localStorage.ManishEarthScript){
        script.src=localStorage.ManishEarthScript;
      }
