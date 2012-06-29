45089359cfafdcc31bbee48f1b57355a44996816
Manish Goregaokar <manishsmail@gmail.com>
2012-06-29 19:24:30 +0530

    +title

--- StackExchange/SpecificStuff/ShareButtons.user.js
+++ StackExchange/SpecificStuff/ShareButtons.user.js
@@ -1,6 +1,6 @@
 ﻿// ==UserScript==
-// @name Super Multicollider Dropdown™ Search
-// @version 0.0.2
+// @name Share buttons
+// @version 0.0.1
 // @author Manish Goregaokar (http://stackapps.com/users/10098/manishearth)
 // @description Allows uploading of linked thumbnails for images
 // @license GNU GPL v3 (http://www.gnu.org/copyleft/gpl.html) 
