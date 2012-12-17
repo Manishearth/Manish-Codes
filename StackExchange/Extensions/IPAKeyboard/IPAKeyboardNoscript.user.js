b4699cc166678e25650b059a40c8d0fd115beef8
Manish Goregaokar <manishsmail@gmail.com>
2012-12-17 11:50:13 +0530

    noscript

--- StackExchange/Extensions/IPAKeyboard/IPAKeyboardNoscript.user.js
+++ StackExchange/Extensions/IPAKeyboard/IPAKeyboardNoscript.user.js
@@ -1,17 +1,19 @@
 // ==UserScript==
 // @name           IPA Keyboard
 // @namespace      Manishearth
+// @require        http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js
 // @description    IPA Keyboard for Linguistics.SE
 // @include        http://linguistics.stackexchange.com/*
 // @author         Manish Goregaokar
 
 // ==/UserScript==
 
+
 (function(){
 	 var d=new Date();
      var script = document.createElement("script");
      script.type = "text/javascript";
-     script.src="https://raw.github.com/Manishearth/Manish-Codes/master/StackExchange/Extensions/IPAKeyboard/IPAKeyboard.js?rand="+d.getMonth()+""+d.getDay();
+     script.src="https://raw.github.com/Manishearth/Manish-Codes/master/StackExchange/Extensions/IPAKeyboard/IPAKeyboardNS.js?rand="+d.getMonth()+""+d.getDay();
      document.body.appendChild(script);
 
 })();
