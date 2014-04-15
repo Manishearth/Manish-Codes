9c196f09bb97ebc6cca990641138edb0ac1bc433
Manish Goregaokar <manishsmail@gmail.com>
2014-04-16 04:35:46 +0530

    up

--- StackExchange/SpecificStuff/MSESkin.user.js
+++ StackExchange/SpecificStuff/MSESkin.user.js
@@ -9,13 +9,4 @@
 // ==/UserScript==
 // See http://meta.stackoverflow.com/questions/158762/whats-up-with-the-new-css
 
-function with_jquery(f) {
-    var script = document.createElement("script");
-    script.type = "text/javascript";
-    script.textContent = "(" + f.toString() + ")(jQuery)";
-    document.body.appendChild(script);
-};
-
-with_jquery(function ($) {
- $('[rel=stylesheet]')[0].href="https://dev.stackoverflow.com/content/stackexchangemeta/all.css";
-});
+document.querySelector('[rel=stylesheet]').href="https://dev.stackoverflow.com/content/stackexchangemeta/all.css";
