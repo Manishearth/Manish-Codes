6673f9e1576619c9ac8f25d9dbabca16278881d3
Vampyricon <39087656+Vampyricon@users.noreply.github.com>
2021-02-01 11:43:09 +0800

    Update ChineseSimplified.user.js

--- RandomUserscripts/ChineseSimplified.user.js
+++ RandomUserscripts/ChineseSimplified.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name Stay in Simplified Chinese
+// @name Stay in Traditional Chinese
 // @version 1.0.0
-// @author Manish Goregaokar (http://stackapps.com/users/10098/manishearth)
-// @description Make Chinese Wikipedia stay in simplified chinese
+// @author Manish Goregaokar (http://stackapps.com/users/10098/manishearth) (edited by Vampyricon for traditional)
+// @description Make Chinese Wikipedia stay in traditional chinese
 // @license GNU GPL v3 (http://www.gnu.org/copyleft/gpl.html) 
 // @include http://zh.wikipedia.org/*
 // ==/UserScript==
@@ -18,13 +18,13 @@ function with_jquery(f) {
 with_jquery(function($){
 
 $('document').ready(function(){
-var re=new RegExp("zh.wikipedia.org\\/zh-cn\\/","g")
+var re=new RegExp("zh.wikipedia.org\\/zh-hk\\/","g")
 if(document.location.href.match(re)){
 re=new RegExp("zh.wikipedia.org\\/wiki\\/","g")
 
 
 $('a').each(function(){
-    this.href=this.href.replace(re,"zh.wikipedia.org/zh-cn/")
+    this.href=this.href.replace(re,"zh.wikipedia.org/zh-hk/")
     
 });
 }
